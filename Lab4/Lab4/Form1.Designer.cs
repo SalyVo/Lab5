6a31bdb1ecd9f4170d4ff499a10dfdf3dcc47c53
Vyacheslav <49403218+SalyVo@users.noreply.github.com>
2019-12-26 19:20:40 +0300

    Add files via upload

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileWindow = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textWord = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listWords = new System.Windows.Forms.ListBox();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuMain
+            // 
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФайлToolStripMenuItem});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Size = new System.Drawing.Size(284, 24);
+            this.menuMain.TabIndex = 0;
+            this.menuMain.Text = "menuStrip1";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл";
+            this.открытьФайлToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время загрузки файла";
+            // 
+            // textFile
+            // 
+            this.textFile.Location = new System.Drawing.Point(142, 33);
+            this.textFile.Name = "textFile";
+            this.textFile.Size = new System.Drawing.Size(130, 20);
+            this.textFile.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Слово";
+            // 
+            // textWord
+            // 
+            this.textWord.Location = new System.Drawing.Point(56, 60);
+            this.textWord.Name = "textWord";
+            this.textWord.Size = new System.Drawing.Size(130, 20);
+            this.textWord.TabIndex = 4;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonSearch.Location = new System.Drawing.Point(192, 59);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(80, 23);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "Найти";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listWords
+            // 
+            this.listWords.FormattingEnabled = true;
+            this.listWords.Location = new System.Drawing.Point(12, 137);
+            this.listWords.Name = "listWords";
+            this.listWords.Size = new System.Drawing.Size(256, 173);
+            this.listWords.TabIndex = 6;
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(126, 316);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(142, 20);
+            this.textSearch.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Время поиска слова";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Макс. расстояние";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "5";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(22, 117);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Дамерау-Левенштейн";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(284, 349);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listWords);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuMain);
+            this.MainMenuStrip = this.menuMain;
+            this.Name = "Form1";
+            this.Text = "Лабораторная №4";
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textWord;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listWords;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Label label3;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        private List<string> strings;
+        private Stopwatch stopWatch;
+        public Form1()
+        {
+            InitializeComponent();
+            openFileWindow.Filter = "Text files(*.txt)|*.txt";
+            openFileWindow.InitialDirectory = Directory.GetCurrentDirectory();
+            stopWatch = new Stopwatch();
+            strings = new List<string>();
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileWindow.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            else
+            {
+                int maxDist = Convert.ToInt32(textBox1.Text);
+
+                stopWatch.Start();
+                // получаем выбранный файл
+                string filename = openFileWindow.FileName;
+                // читаем файл в строку
+                string fileText = System.IO.File.ReadAllText(filename, Encoding.UTF8);
+                char[] separator = { ',', ' ', '?', '!' };
+                string[] words = fileText.Split(separator);
+                foreach(string word in words)
+                {
+                    // if(!strings.Contains(word))
+                    bool fl = false;
+
+                    foreach ( var s in strings )
+                        if (levensteinDist.calcLevenstein( word, s, checkBox1.Checked) <= maxDist)
+                        {
+                            fl = true;
+                            break;
+
+                        }
+
+                    if (fl)
+                        strings.Add(word);
+                }
+                stopWatch.Stop();
+                textFile.Text = stopWatch.ElapsedMilliseconds + " миллисекунд.";
+                MessageBox.Show("Файл успешно считан.");
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            listWords.Items.Clear();
+            string search = textWord.Text;
+            stopWatch.Start();
+            foreach(string word in strings)
+            {
+                if(word.Contains(search))
+                {
+                    listWords.BeginUpdate();
+                    listWords.Items.Add(word);
+                    listWords.EndUpdate();
+                }
+            }
+            stopWatch.Stop();
+            textSearch.Text = stopWatch.ElapsedMilliseconds + " миллисекунд.";
+        }
+
+    }
+}
